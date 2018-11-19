1051de1ce47a17aa82f5bb359f55a58f0dc82b0d
variable56 <variable56@gmail.com>
2018-11-19 08:44:35 -0700

    psedocode and html skeleton

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//Create a landing page that will show a styled button and background
+
+//Create a question bank as an array of objects that have questions and answers along with correct answers
+
+//setup the game with variables correct, incorrect, missed
+
+//Click event that will trigger the 1st question selection and then start a timer.
+
+//Display a countdown timer on screen that shows the player how much time they have to answer the question
+
+//when a question is answered a new question is displayed and the timer starts over.
+
+
